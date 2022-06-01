6900b6172e68abda083febaaaf3e258e5bfb2d34
ShyZzz <2497141901@qq.com>
2022-06-01 21:30:28 +0800

    shy

--- src/leecode/slidingWindow/无重复最长子串方法2.java
+++ src/leecode/slidingWindow/无重复最长子串方法2.java
@@ -18,21 +18,22 @@ public class 无重复最长子串方法2 {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        if (s == null || s.length() == 0) {
+        int n = s.length();
+        if (s == null || n == 0) {
             return 0;
         }
-        int dp = 0;// dp表示的是以s[i]结尾且最长无重复子串开始的下标
-        int res = 0;
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
+        int dp = 0;// dp[i]表示以s[i]结尾的最长无重复子串开始的下标
+        int ans = 0;
+        Map<Character, Integer> map = new HashMap<>();// 用来存储s[i]在[dp[i-1],i-1]出现的最后下标
+        for (int i = 0; i < n; i++) {
             char c = s.charAt(i);
             Integer repeatIndex = map.get(c);
-            if (repeatIndex != null && repeatIndex >= dp) {
+            if (repeatIndex != null && repeatIndex >= dp) {// s[i]在[dp[i-1],i-1]出现了，那么dp[i]=repeatIndex+1;如果s[i]未在[0,i-1]出现或者在[0,dp[i-1]]出现，那么dp[i]=dp[i-1];
                 dp = repeatIndex + 1;
             }
-            res = Math.max(res, i - dp + 1);
+            ans = Math.max(ans, i - dp + 1);
             map.put(c, i);
         }
-        return res;
+        return ans;
     }
 }
