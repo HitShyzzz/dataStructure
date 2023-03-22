16d7fe855e01da58351f37fa0dcf526ffa188182
ShyZzz <2497141901@qq.com>
2023-03-23 00:57:19 +0800

    3.22 commit 8

---
+++ src/solution/Hello.java
@@ -0,0 +1,16 @@
+package solution;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: shy
+ * @Date: 2023/03/23/0:45
+ * @Description:
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("宋海洋");
+
+    }
+}
