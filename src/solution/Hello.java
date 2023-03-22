389b10d566b5e87296387593b229c1be9070103e
HitShyzzz <90769624+HitShyzzz@users.noreply.github.com>
2023-03-23 01:00:34 +0800

    Update Hello.java
    origin commit 2

--- src/solution/Hello.java
+++ src/solution/Hello.java
@@ -10,5 +10,6 @@ package solution;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
